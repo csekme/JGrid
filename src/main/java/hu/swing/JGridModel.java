e2b171983f5066132569712141dacedd8db7f255
Csekme Krisztián <csekme.krisztian@outlook.com>
2021-06-25 07:10:37 +0200

    Initial commit with first class JGridModel added.

---
+++ src/main/java/hu/swing/JGridModel.java
@@ -0,0 +1,123 @@
+package hu.swing;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableModel;
+
+/**
+ * JGridModel is represent of the data model for JGrid.
+ * @author Ballonyi, Csekme
+ */
+public class JGridModel implements TableModel {
+
+    /**
+     * Returns the number of rows in the model.
+     * A JGrid uses this method to determine how many rows it should display.
+     * This method should be quick, as it is called frequently during rendering.
+     * @return the number of rows in the model
+     */
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    /**
+     * Returns the number of columns in the model. A
+     * <code>JGrid</code> uses this method to determine how many columns it
+     * should create and display by default.
+     * @return the number of columns in the model
+     */
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    /**
+     * Returns the name of the column at <code>columnIndex</code>.  This is used
+     * to initialize the table's column header name.  Note: this name does
+     * not need to be unique; two columns in a table can have the same name.
+     *
+     * @param   columnIndex     the index of the column
+     * @return  the name of the column
+     */
+    @Override
+    public String getColumnName(int columnIndex) {
+        return null;
+    }
+
+    /**
+     * Returns the most specific superclass for all the cell values
+     * in the column.  This is used by the <code>JGrid</code> to set up a
+     * default renderer and editor for the column.
+     *
+     * @param columnIndex  the index of the column
+     * @return the common ancestor class of the object values in the model.
+     */
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return null;
+    }
+
+    /**
+     * Returns true if the cell at <code>rowIndex</code> and
+     * <code>columnIndex</code>
+     * is editable.  Otherwise, <code>setValueAt</code> on the cell will not
+     * change the value of that cell.
+     *
+     * @param   rowIndex        the row whose value to be queried
+     * @param   columnIndex     the column whose value to be queried
+     * @return  true if the cell is editable
+     */
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+    /**
+     * Returns the value for the cell at <code>columnIndex</code> and
+     * <code>rowIndex</code>.
+     *
+     * @param   rowIndex        the row whose value is to be queried
+     * @param   columnIndex     the column whose value is to be queried
+     * @return  the value Object at the specified cell
+     */
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
+
+    /**
+     * Sets the value in the cell at <code>columnIndex</code> and
+     * <code>rowIndex</code> to <code>aValue</code>.
+     *
+     * @param   aValue           the new value
+     * @param   rowIndex         the row whose value is to be changed
+     * @param   columnIndex      the column whose value is to be changed
+     * @see #getValueAt
+     * @see #isCellEditable
+     */
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+
+    }
+
+    /**
+     * Adds a listener to the list that is notified each time a change
+     * to the data model occurs.
+     *
+     * @param l the TableModelListener
+     */
+    @Override
+    public void addTableModelListener(TableModelListener l) {
+
+    }
+
+    /**
+     * Removes a listener from the list that is notified each time a
+     * change to the data model occurs.
+     *
+     * @param l the TableModelListener
+     */
+    @Override
+    public void removeTableModelListener(TableModelListener l) {
+
+    }
+}
---
+++ src/test/java/Sample.java
@@ -0,0 +1,10 @@
+import javax.swing.table.DefaultTableModel;
+
+public class Sample {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
